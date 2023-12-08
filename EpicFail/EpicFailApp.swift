a443b1344bf779f8282dcb40dc9e5db1266e0adc
Nick Thompson <git@nickt.dev>
2023-12-07 20:28:47 -0500

    Initial Commit

---
+++ EpicFail/EpicFailApp.swift
@@ -0,0 +1,17 @@
+//
+//  EpicFailApp.swift
+//  EpicFail
+//
+//  Created by Nick Thompson on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct EpicFailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
